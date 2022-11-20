46a2c1f53a945c558ab597b200262013c639b366
Bartosz Mróz <bartosz.mroz@ecom.software>
2022-11-20 11:36:24 +0100

    Api + interceptors

---
+++ src/api/UserApi.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+import { authorizedApi } from "../hooks/useAxios";
+import { User } from "../models/User";
+
+export class UserApi {
+  static getUser = async () => await authorizedApi.get<User>("/profile");
+}
